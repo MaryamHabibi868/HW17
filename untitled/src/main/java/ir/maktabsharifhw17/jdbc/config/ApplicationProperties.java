a6471f106b039a314e10a922218adb4a311a511e
Maryam <maryamhabibi868@gmail.com>
2025-04-14 23:54:53 +0330

    WIP: Add config package with two setup classes

---
+++ untitled/src/main/java/ir/maktabsharifhw17/jdbc/config/ApplicationProperties.java
@@ -0,0 +1,13 @@
+package ir.maktabsharifhw17.jdbc.config;
+
+public class ApplicationProperties {
+
+    public static final String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String JDBC_USER = "postgres";
+    public static final String JDBC_PASSWORD = "admin";
+
+    private ApplicationProperties() {
+    }
+
+
+}
