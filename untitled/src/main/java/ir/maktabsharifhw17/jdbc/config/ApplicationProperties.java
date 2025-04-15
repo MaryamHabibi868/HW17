5a9524abe5d69a3db8a9a704c31618184832df2d
Maryam <maryamhabibi868@gmail.com>
2025-04-15 23:53:45 +0330

    update ApplicationProperties class

--- untitled/src/main/java/ir/maktabsharifhw17/jdbc/config/ApplicationProperties.java
+++ untitled/src/main/java/ir/maktabsharifhw17/jdbc/config/ApplicationProperties.java
@@ -2,12 +2,10 @@ package ir.maktabsharifhw17.jdbc.config;
 
 public class ApplicationProperties {
 
-    public static final String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String JDBC_URL = "jdbc:postgresql://localhost:5432/hw17";
     public static final String JDBC_USER = "postgres";
     public static final String JDBC_PASSWORD = "admin";
 
     private ApplicationProperties() {
     }
-
-
 }
