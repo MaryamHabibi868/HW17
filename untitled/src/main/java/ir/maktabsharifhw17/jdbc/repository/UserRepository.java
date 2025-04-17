2220b44e9be01cc24270fa6a4ad23b62dbeceff6
Maryam <maryamhabibi868@gmail.com>
2025-04-17 21:34:26 +0330

    WIP: update UserRepository

--- untitled/src/main/java/ir/maktabsharifhw17/jdbc/repository/UserRepository.java
+++ untitled/src/main/java/ir/maktabsharifhw17/jdbc/repository/UserRepository.java
@@ -3,6 +3,12 @@ package ir.maktabsharifhw17.jdbc.repository;
 import ir.maktabsharifhw17.jdbc.domains.User;
 import ir.maktabsharifhw17.jdbc.repository.base.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends
         CrudRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+
+    Optional<User> login(String userName, String password);
+
 }
